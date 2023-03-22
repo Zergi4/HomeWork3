1c9346d740368e436b1204c9b1bf95244dfb54c1
Zergi4 <ksyssau@ya.ru>
2023-03-22 23:14:44 +0300

    4 задача

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,14 @@ public class Main {
         long d = 111;
         float e = 3.14f;
         double f = 3.33333333;
+        System.out.println(" Задача 1");
         System.out.println("Значение переменной a c типом byte равно " + a);
         System.out.println("Значение переменной b c типом short равно " + b);
         System.out.println("Значение переменной c c типом int равно " + c);
         System.out.println("Значение переменной d c типом long равно " + d);
         System.out.println("Значение переменной e c типом float равно " + e);
         System.out.println("Значение переменной f c типом double равно " + f); // задача1
-
+        System.out.println(" Задача 2");
         float a2 = 27.12f;
         long b2 = 987678965549L;
         double c2 = 2.786;
@@ -21,16 +22,23 @@ public class Main {
         short e2 = -159;
         short f2 = 27897;
         byte g2 = 67; //задача 2
-
+        System.out.println(" Задача 3");
         byte studentLp = 23;
         byte studentAs = 27;
         byte studentEa = 30;
-        short summListov = 480;
+        float summListov = 480;
         float listovNaodnogo = summListov / (studentLp+studentEa+studentAs);
         System.out.println("На каждого ученика рассчитано " + listovNaodnogo + " листов бумаги"); //задача3
-
-
-
+        System.out.println(" Задача 4");
+        byte efficiencyMachine = 8;
+        int bottles1 = 20 * efficiencyMachine;
+        int bottles2 = 24 * 60 * efficiencyMachine;
+        int bottles3 = 3 * 24 * 60 * efficiencyMachine;
+        int bottles4 = 30 * 24 * 60 * efficiencyMachine;
+        System.out.println(" За 20 минут машина произвела " + bottles1 + " штук бутылок");
+        System.out.println(" За сутки машина произвела " + bottles2 + " штук бутылок");
+        System.out.println(" За 3 суток машина произвела " + bottles3 + " штук бутылок");
+        System.out.println(" За 1 месяц машина произвела " + bottles4 + " штук бутылок");
 
 
     }
